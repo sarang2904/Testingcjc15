d77d408818b363a0873433e8c73d585a59c0b7ac
sarang2904 <107532296+sarang2904@users.noreply.github.com>
2022-06-15 12:54:02 +0530

    Update Newdemo.java

--- Gitproject/src/Gitpackg/Newdemo.java
+++ Gitproject/src/Gitpackg/Newdemo.java
@@ -4,6 +4,9 @@ public class Newdemo {
 public void m1() {
 	System.out.println("M1");
 public void m2()	{
-System.out.println("M2")	
+System.out.println("M2");	
 }
-}
+	public void m3()
+	{
+	System.out.println("M3");	
+	}
